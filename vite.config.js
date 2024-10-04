baf5b4560c91eed7332f8f0c5aefa85cae44dfdd
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-10-04 19:35:26 +0530

    vite config.js /api added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 
+  proxy: {
+    '/api': {
+      target: 'https://ecn-backend.onrender.com',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
+    },
+  },
+
   
   
   plugins: [react()],
